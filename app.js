14fb74ee37f1d498db1890bc71fd3954fa085f3a
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-03-25 10:35:29 -0300

    Módulo fs sync

--- app.js
+++ app.js
@@ -4,28 +4,19 @@ const fs = require("fs");
 
 console.log("Antes de leer el archivo...");
 
-fs.readFile("./index.html", "utf-8", (err, contenido) => {
-    if (err) {
-        throw err;
-    }
-    console.log("mensaje...");
-});
+const archivo = fs.readFileSync("./index.html", "utf-8");
+
+console.log(archivo);
 
 console.log("Después de leer el archivo...");
 
 // Cambiar el nombre de un archivo.
-fs.rename("./index.html", "./main.html", (err) => {
-    if (err) {
-        throw err;
-    }
-
-    console.log("Nombre cambiado exitosamente.");
-});
+fs.renameSync("./index.html", "./main.html");
 
 console.log("Después de cambiar el nombre del archivo...");
 
 // Agregar contenido al final de un archivo.
-fs.appendFile("./index.html", "<p>Hola</p>", (err) => {
+fs.appendFileSync("./index.html", "<p>Hola</p>", (err) => {
     if (err) {
         throw err;
     }
@@ -36,7 +27,7 @@ fs.appendFile("./index.html", "<p>Hola</p>", (err) => {
 console.log("Después de agregar contenido al archivo...");
 
 // Reemplazar todo el contenido el archivo.
-fs.writeFile("./index.html", "Contenido Nuevo", (err) => {
+fs.writeFileSync("./index.html", "Contenido Nuevo", (err) => {
     if (err) {
         throw err;
     }
@@ -47,7 +38,7 @@ fs.writeFile("./index.html", "Contenido Nuevo", (err) => {
 console.log("Después de reemplazar todo el contenido al archivo...");
 
 // Eliminar archivos
-fs.unlink("./main.html", (err) => {
+fs.unlinkSync("./main.html", (err) => {
     if (err) {
         throw err;
     }
