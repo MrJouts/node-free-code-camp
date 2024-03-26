abe7a249c5ea8d968fa6bda6628a593cb64947bc
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-03-26 13:26:41 -0300

    Usando async await

--- app.js
+++ app.js
@@ -26,15 +26,29 @@ function procesarPedido(respuesta) {
 }
 
 // Encadenamos promesas
-ordenarProducto("lapiz")
-    .then((respuesta) => {
+// ordenarProducto("taza")
+//     .then((respuesta) => {
+//         console.log("Respuesta recibida");
+//         console.log(respuesta);
+//         return procesarPedido(respuesta);
+//     })
+//     .then((respuestaProcesada) => {
+//         console.log(respuestaProcesada);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+// Async / await
+async function realizarPedido(producto) {
+    try {
+        const respuesta = await ordenarProducto(producto);
         console.log("Respuesta recibida");
-        console.log(respuesta);
-        return procesarPedido(respuesta);
-    })
-    .then((respuestaProcesada) => {
+        const respuestaProcesada = await procesarPedido(respuesta);
         console.log(respuestaProcesada);
-    })
-    .catch((error) => {
+    } catch (error) {
         console.log(error);
-    });
+    }
+}
+
+realizarPedido("taza");
