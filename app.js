4db6dbe653c37bdf1c8f5b22b51babbdb24f8e67
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-05 18:23:50 -0300

    Routing en Node.js parte 1

--- app.js
+++ app.js
@@ -1,10 +1,78 @@
-const miURL = new URL(
-    "https://www.ejemplo.org/cursos/programacion?ordenar=vistas&nivel=1"
-);
+const http = require("http");
+const cursos = require("./cursos");
 
-console.log(miURL.hostname); // www.ejemplo.com
-console.log(miURL.pathname); // cursos/programacion
+const servidor = http.createServer((req, res) => {
+    const { method } = req;
 
-console.log(miURL.searchParams); // URLSearchParams { 'ordenar' => 'vistas', 'nivel' => '1' }
-console.log(miURL.searchParams.get("ordenar")); // vistas
-console.log(miURL.searchParams.get("nivel")); // 1
+    switch (method) {
+        case "GET":
+            return manejarSolicitudGET(req, res);
+        case "POST":
+            return manejarSolicitudPOST(req, res);
+        case "PUT":
+            return manejarSolicitudPUT(req, res);
+        case "DELETE":
+            return manejarSolicitudDELETE(req, res);
+        default:
+            console.log(
+                `El método no puede ser manejado por el servidor: ${method}`
+            );
+    }
+});
+
+function manejarSolicitudGET(req, res) {
+    const path = req.url;
+
+    if (path === "/") {
+        res.statusCode = 200;
+        res.end("Bienvenidos a mi primer servidor y API creados con Node.js");
+    } else if (path === "/cursos") {
+        res.statusCode = 200;
+        res.end(JSON.stringify(cursos.infoCursos));
+    } else if (path === "/cursos/programacion") {
+        res.statusCode === 200;
+        res.end(JSON.stringify(cursos.infoCursos.programacion));
+    } else {
+        res.statusCode = 404;
+        res.end("El recurso solicitado no existe...");
+    }
+}
+
+function manejarSolicitudPOST(req, res) {
+    const path = req.url;
+
+    if (path === "/cursos/programacion") {
+        res.statusCode = 200;
+        res.end(
+            "El servidor recibió una solicitud POST para /cursos/programacion"
+        );
+    }
+}
+
+function manejarSolicitudPUT(req, res) {
+    const path = req.url;
+
+    if (path === "/cursos/programacion/1") {
+        res.statusCode = 200;
+        res.end(
+            "El servidor recibió una solicitud PUT para /cursos/programacion/1"
+        );
+    }
+}
+
+function manejarSolicitudDELETE(req, res) {
+    const path = req.url;
+
+    if (path === "/cursos/programacion/1") {
+        res.statusCode = 200;
+        res.end(
+            "El servidor recibió una solicitud DELETE para /cursos/programacion/1"
+        );
+    }
+}
+
+const PUERTO = 3000;
+
+servidor.listen(PUERTO, () => {
+    console.log(`El servidor está escuchando en el puerto ${PUERTO}...`);
+});
---
+++ cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+    programacion: [
+        {
+            id: 1,
+            titulo: "Aprende Python",
+            lenguaje: "python",
+            vistas: 15000,
+            nivel: "basico",
+        },
+        {
+            id: 2,
+            titulo: "Python intermedio",
+            lenguaje: "python",
+            vistas: 13533,
+            nivel: "intermedio",
+        },
+        {
+            id: 2,
+            titulo: "Aprende JavaScript intermedio",
+            lenguaje: "javascript",
+            vistas: 102333,
+            nivel: "basico",
+        },
+    ],
+    matematicas: [
+        {
+            id: 1,
+            titulo: "Aprende Calculo",
+            tema: "calculo",
+            visitas: 12427,
+            nivel: "basico",
+        },
+        {
+            id: 2,
+            titulo: "Aprende Algebra",
+            tema: "algebra",
+            visitas: 15722,
+            nivel: "intermedio",
+        },
+    ],
+};
+
+module.exports.infoCursos = infoCursos;
