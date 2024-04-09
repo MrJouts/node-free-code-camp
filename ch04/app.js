8ce3ea3bcb7ad03e7d39742258010433529eed61
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-09 18:35:07 -0300

    Agregar rutas en Express

--- ch04/app.js
+++ ch04/app.js
@@ -3,4 +3,26 @@ const app = express();
 
 const { infoCursos } = require("./cursos");
 
+// Routing
 
+app.get("/", (req, res) => {
+    res.send("Mi primer servidor. Cursos 💻.");
+});
+
+app.get("/api/cursos", (req, res) => {
+    res.send(JSON.stringify(infoCursos));
+});
+
+app.get("/api/cursos/programacion", (req, res) => {
+    res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get("/api/cursos/matematicas", (req, res) => {
+    res.send(infoCursos.matematicas);
+});
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+    console.log(`El servidor está escuchando en el puerto ${PUERTO}...`);
+});
