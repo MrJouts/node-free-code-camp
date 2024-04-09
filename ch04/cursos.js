06ba5e41af2a12c3a4cc263c2ec292a9a7137d7d
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-09 11:40:35 -0300

    Crear un proyecto con Express

---
+++ ch04/cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+    programacion: [
+        {
+            id: 1,
+            titulo: "Aprende Python",
+            tema: "python",
+            vistas: 15000,
+            nivel: "basico",
+        },
+        {
+            id: 2,
+            titulo: "Python Intermedio",
+            tema: "python",
+            vistas: 13533,
+            nivel: "intermedio",
+        },
+        {
+            id: 3,
+            titulo: "Aprende Javascript",
+            tema: "javascript",
+            vistas: 102223,
+            nivel: "basico",
+        },
+    ],
+    matematicas: [
+        {
+            id: 1,
+            titulo: "Aprende Calculo",
+            tema: "calculo",
+            vistas: 12427,
+            nivel: "basico",
+        },
+        {
+            id: 2,
+            titulo: "Aprende Algebra",
+            tema: "algebra",
+            vistas: 15722,
+            nivel: "intermedio",
+        },
+    ],
+};
+
+module.exports.infoCursos = infoCursos;
