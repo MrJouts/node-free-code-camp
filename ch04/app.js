b360bae759796d6b25fcdfc6cbb7b4fa1f804df3
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-10 10:43:57 -0300

    Separar routers en diferentes archivos

--- ch04/app.js
+++ ch04/app.js
@@ -1,84 +1,22 @@
 const express = require("express");
 const app = express();
 
-const { infoCursos } = require("./cursos");
+const { infoCursos } = require("./datos/cursos");
 
 // Routers
-const routerProgramacion = express.Router();
+const routerProgramacion = require("./routers/programacion");
 app.use("/api/cursos/programacion", routerProgramacion);
 
-const routerMatematicas = express.Router();
+const routerMatematicas = require("./routers/matematicas");
 app.use("/api/cursos/matematicas", routerMatematicas);
 
 // Routing
-
 app.get("/", (req, res) => {
     res.send("Mi primer servidor. Cursos 💻.");
 });
 
 app.get("/api/cursos", (req, res) => {
     res.send(JSON.stringify(infoCursos));
-});
-
-// Prgramación
-
-routerProgramacion.get("/", (req, res) => {
-    res.send(JSON.stringify(infoCursos.programacion));
-});
-
-routerProgramacion.get("/:lenguaje", (req, res) => {
-    const lenguaje = req.params.lenguaje;
-    const resultados = infoCursos.programacion.filter(
-        (curso) => curso.lenguaje === lenguaje
-    );
-
-    if (resultados.length === 0) {
-        return res.status(404).send(`No se encontraron cursos de ${lenguaje}.`);
-    }
-
-    if (req.query.ordenar === "vistas") {
-        return res.send(
-            JSON.stringify(resultados.sort((a, b) => a.vistas - b.vistas))
-        );
-    }
-
-    res.send(JSON.stringify(resultados));
-});
-
-routerProgramacion.get("/:lenguaje/:nivel", (req, res) => {
-    const lenguaje = req.params.lenguaje;
-    const nivel = req.params.nivel;
-
-    const resultados = infoCursos.programacion.filter(
-        (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel
-    );
-
-    if (resultados.length === 0) {
-        return res
-            .status(404)
-            .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
-    }
-
-    res.send(JSON.stringify(resultados));
-});
-
-// Matemáticas
-
-routerMatematicas.get("/", (req, res) => {
-    res.send(infoCursos.matematicas);
-});
-
-routerMatematicas.get("/:tema", (req, res) => {
-    const tema = req.params.tema;
-    const resultados = infoCursos.matematicas.filter(
-        (curso) => curso.tema === tema
-    );
-
-    if (resultados.length === 0) {
-        return res.status(404).send(`No se encontraron cursos de ${tema}.`);
-    }
-
-    res.send(JSON.stringify(resultados));
 });
 
 const PUERTO = process.env.PORT || 3000;
---
+++ ch04/routers/programacion.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const routerProgramacion = express.Router();
+
+const { programacion } = require("../datos/cursos").infoCursos;
+
+routerProgramacion.get("/", (req, res) => {
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.get("/:lenguaje", (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const resultados = programacion.filter(
+        (curso) => curso.lenguaje === lenguaje
+    );
+
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}.`);
+    }
+
+    if (req.query.ordenar === "vistas") {
+        return res.send(
+            JSON.stringify(resultados.sort((a, b) => a.vistas - b.vistas))
+        );
+    }
+
+    res.send(JSON.stringify(resultados));
+});
+
+routerProgramacion.get("/:lenguaje/:nivel", (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+
+    const resultados = programacion.filter(
+        (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel
+    );
+
+    if (resultados.length === 0) {
+        return res
+            .status(404)
+            .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+    }
+
+    res.send(JSON.stringify(resultados));
+});
+
+module.exports = routerProgramacion;
