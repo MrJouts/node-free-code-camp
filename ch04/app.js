2cef311dcbd9cd8860aedabb4093608d1a80b1b4
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-10 10:00:26 -0300

    Parametros de Ruta

--- ch04/app.js
+++ ch04/app.js
@@ -17,6 +17,53 @@ app.get("/api/cursos/programacion", (req, res) => {
     res.send(JSON.stringify(infoCursos.programacion));
 });
 
+// Prgramación
+
+app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const resultados = infoCursos.programacion.filter(
+        (curso) => curso.lenguaje === lenguaje
+    );
+
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}.`);
+    }
+
+    res.send(JSON.stringify(resultados));
+});
+
+app.get("/api/cursos/programacion/:lenguaje/:nivel", (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+
+    const resultados = infoCursos.programacion.filter(
+        (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel
+    );
+
+    if (resultados.length === 0) {
+        return res
+            .status(404)
+            .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+    }
+
+    res.send(JSON.stringify(resultados));
+});
+
+// Matemáticas
+
+app.get("/api/cursos/matematicas/:tema", (req, res) => {
+    const tema = req.params.tema;
+    const resultados = infoCursos.matematicas.filter(
+        (curso) => curso.tema === tema
+    );
+
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${tema}.`);
+    }
+
+    res.send(JSON.stringify(resultados));
+});
+
 app.get("/api/cursos/matematicas", (req, res) => {
     res.send(infoCursos.matematicas);
 });
