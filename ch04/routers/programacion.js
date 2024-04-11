06687885caea57753e6ca44344920fe171d17c1f
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-11 19:18:38 -0300

    POST, PUT, PATCH and DELETE

--- ch04/routers/programacion.js
+++ ch04/routers/programacion.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
-const routerProgramacion = express.Router();
-
 const { programacion } = require("../datos/cursos").infoCursos;
 
+const routerProgramacion = express.Router();
+
+// Middleware
+routerProgramacion.use(express.json());
+
 routerProgramacion.get("/", (req, res) => {
-    res.send(JSON.stringify(programacion));
+    // res.send(JSON.stringify(programacion));
+    res.json(programacion);
 });
 
 routerProgramacion.get("/:lenguaje", (req, res) => {
@@ -36,12 +40,58 @@ routerProgramacion.get("/:lenguaje/:nivel", (req, res) => {
     );
 
     if (resultados.length === 0) {
-        return res
-            .status(404)
-            .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+        return (
+            res
+                // .status(404)
+                .status(204) // Código de error para informar que no se encontró contenido 
+                // .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+                .end()
+        );
     }
 
     res.send(JSON.stringify(resultados));
+});
+
+routerProgramacion.post("/", (req, res) => {
+    let cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.put("/:id", (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex((curso) => curso.id == id);
+
+    if (indice >= 0) {
+        programacion[indice] = cursoActualizado;
+    }
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.patch("/:id", (req, res) => {
+    const infoActualizada = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex((curso) => curso.id == id);
+
+    if (indice >= 0) {
+        const cursoAModificar = programacion[indice];
+        Object.assign(cursoAModificar, infoActualizada);
+    }
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.delete("/:id", (req, res) => {
+    const id = req.params.id;
+
+    const indice = programacion.findIndex((curso) => curso.id == id);
+
+    if (indice >= 0) {
+        programacion.splice(indice, 1);
+    }
+    res.send(JSON.stringify(programacion));
 });
 
 module.exports = routerProgramacion;
